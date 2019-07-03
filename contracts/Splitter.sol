7f2bd74da2c68adea495b5381878223e53432459
Luke Donato <lukedonato@gmail.com>
2019-07-03 00:17:45 +0000

    more initial setup

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,21 +1,24 @@
-pragma solidity ^0.5.2;
+pragma solidity 0.5.0;
 
 contract Splitter {
     address public aliceAddress;
-    address public bobAddress;
-    address public carolAddress;
+    address payable public address1;
+    address payable public address2;
 
-    constructor(address bobAddress, address carolAddress) public {
+    event Transfer(uint256 indexed _aliceBalance, uint256 indexed _bobBalance, uint256 _CarolBalance);
+    
+    constructor(address payable bobAddress, address payable carolAddress) public {
         // assume alice will deploy the contract
         aliceAddress = msg.sender;
-        bobAddress = bobAddress;
-        carolAddress = carolAddress;
+        address1 = bobAddress;
+        address2 = carolAddress;
 
     }
 
-    function() payable public {
-        require(msg.sender == owner);
-        bobAddress.transfer(msg.value / 2);
-        carolAddress.transfer(msg.value / 2);
+    function() payable external {
+        require(msg.sender == aliceAddress);
+        address1.transfer(msg.value / 2);
+        address2.transfer(msg.value / 2);
+        emit Transfer(aliceAddress.balance, address1.balance, address2.balance);
     }
 }
